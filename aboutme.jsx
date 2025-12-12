19c3ebbf7f5bc2b7a56852a35f0fb963f1ec445d
immehedi49 <m.hasanrafin49@gmail.com>
2025-12-12 12:42:55 +0600

    data analyst portfolio

---
+++ aboutme.jsx
@@ -0,0 +1,33 @@
+export default function AboutMe() {
+    return (
+    <div className="aboutme">
+            <div className="aboutme-container">
+                <div className="aboutme-title">About Me » </div>
+                <div className="title-line"></div>
+            </div>
+            <div className="aboutme-intro">It`s me Md. Mehedi Hasan, A motivated data analyst looking for an opportunity to utilize my skills in Python, SQL, Excel, and Power BI to contribute to solving business problems, creating effective dashboards, and improving organizational performance in a dynamic environment.</div>
+            <div className="technology-title">Technology Stack</div>
+            <div className="technology-list">
+                <div className="technology-item">
+                    <p> » Python</p>
+                </div>
+                <div className="technology-item">
+                    <p> » Power BI</p>
+                </div>
+                <div className="technology-item">
+                    <p> » PostgreSQL, MySQL</p>
+                </div>
+                <div className="technology-item">
+                    <p> » JavaScript, React JS, HTML</p>
+                </div>
+                <div className="technology-item">
+                    <p> » CSS3, Tailwind CSS, Bootstrap</p>
+                </div>
+                <div className="technology-item">
+                    <p> » Git, Figma</p>
+                </div>
+            </div>
+    </div>
+    )
+        
+}
---
+++ project.jsx
@@ -0,0 +1,118 @@
+export default function Project() {
+    return (
+        <div className="education">
+            <div className="aboutme-container">
+                <div className="aboutme-title">My Creations » </div>
+                <div className="title-line"></div>
+            </div>
+
+            
+
+        <div className="project-list">
+            <div className="project-item">
+                <div className="project-img">
+                    <img src="/assets/human-behavior.png" alt="" />
+                </div>
+                <div className="project-bar">
+                <div>
+                    <a href="https://github.com/immehedi49/human-behavior" target="_blank" rel="noopener noreferrer">
+                        <img className="project-link-img" src="/assets/project link.png" alt="" />
+                    </a>
+                </div>
+                <div>
+                    <p className="project-name">Human Behavior Analysis & Insights</p>
+                    <p className="project-user-tools"> <span>Stack: </span>  Python</p>
+                </div>
+                </div>
+            </div>
+            
+            <div className="project-item">
+                <div className="project-img">
+                    <img src="/assets/diwali-sales.png" alt="" />
+                </div>
+                <div className="project-bar">
+                <div>
+                    <a href="https://github.com/immehedi49/diwali-sales-trend" target="_blank" rel="noopener noreferrer">
+                        <img className="project-link-img" src="/assets/project link.png" alt="" />
+                    </a>
+                </div>
+                <div>
+                    <p className="project-name">Diwali Sales Trend Analysis</p>
+                    <p className="project-user-tools"> <span>Stack: </span> Python</p>
+                </div>
+                </div>
+            </div>
+
+
+            <div className="project-item">
+                <div className="project-img">
+                    <img src="/assets/retail-sales.png" alt="" />
+                </div>
+                <div className="project-bar">
+                <div>
+                    <a href="https://github.com/immehedi49/retail-sales" target="_blank" rel="noopener noreferrer">
+                        <img className="project-link-img" src="/assets/project link.png" alt="" />
+                    </a>
+                </div>               
+                <div>
+                    <p className="project-name">Retail Sales  Analysis & Insights</p>
+                    <p className="project-user-tools"> <span>Stack: </span> SQL</p>
+                </div>
+                </div>
+            </div>
+
+            <div className="project-item">
+                <div className="project-img">
+                    <img src="/assets/walmart.png" alt="" />
+                </div>
+                <div className="project-bar">
+                <div>
+                    <a href="https://github.com/immehedi49/walmart-sales" target="_blank" rel="noopener noreferrer">
+                        <img className="project-link-img" src="/assets/project link.png" alt="" />
+                    </a>
+                </div>
+                <div>
+                    <p className="project-name">Walmart Sales Performance Analysis</p>
+                    <p className="project-user-tools"> <span>Stack: </span> SQL, Python</p>
+                </div>
+                </div>
+            </div>
+
+            <div className="project-item">
+                <div className="project-img">
+                    <img src="/assets/hr.png" alt="" />
+                </div>
+                <div className="project-bar">
+                <div>
+                    <a href="https://github.com/immehedi49/hr-performance-dashboard" target="_blank" rel="noopener noreferrer">
+                        <img className="project-link-img" src="/assets/project link.png" alt="" />
+                    </a>
+                </div>
+                <div>
+                    <p className="project-name">HR Performance Dashboard</p>
+                    <p className="project-user-tools"> <span>Stack: </span>  Power BI, SQL</p>
+                </div>
+                </div>
+            </div>
+
+            <div className="project-item">
+                <div className="project-img">
+                    <img src="/assets/grocery.png" alt="" />
+                </div>
+                <div className="project-bar">
+                <div>
+                    <a href="https://github.com/immehedi49/blinkit-grocery-shop" target="_blank" rel="noopener noreferrer">
+                        <img className="project-link-img" src="/assets/project link.png" alt="" />
+                    </a>
+                </div>
+                <div>
+                    <p className="project-name">Grocery Sales Dashboard</p>
+                    <p className="project-user-tools"> <span>Stack: </span> Power BI, Python, SQL</p>
+                </div>
+                </div>
+            </div>
+        </div>    
+
+        </div>
+    )
+}
