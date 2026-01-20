80d1137e82b6eaa6316b8b449af212fab6d4fcea
immehedi49 <m.hasanrafin49@gmail.com>
2026-01-20 21:18:19 +0600

    cv update

--- home.jsx
+++ home.jsx
@@ -1,14 +1,29 @@
 export default function Home() {
-    return (
-        <div className="home">
-            <div className="hi">Hi,it`s me</div>
-            <div className="myname">Md. Mehedi Hasan</div>
-            <div className="intro-title">Turning Data Into Meaningful Insights</div>
-            <div className="intro">I am a <span className="span">Data Analyst</span> focused on transforming raw data into meaningful insights that drive informed decisions. I specialize in analyzing trends, building clear visualizations, and turning complex information into actionable strategies that help businesses grow smarter and faster.</div>
-            <div className="button-container">
-                <button className="resume-button" onClick={() => window.open("https://drive.google.com/file/d/17Eo9AAwCwTya1uK-XhTF3AhfaYLR_qka/view?usp=sharing", "_blank")} >
-                Check my resume</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="home">
+      <div className="hi">Hi,it`s me</div>
+      <div className="myname">Md. Mehedi Hasan</div>
+      <div className="intro-title">Turning Data Into Meaningful Insights</div>
+      <div className="intro">
+        I am a <span className="span">Data Analyst</span> focused on
+        transforming raw data into meaningful insights that drive informed
+        decisions. I specialize in analyzing trends, building clear
+        visualizations, and turning complex information into actionable
+        strategies that help businesses grow smarter and faster.
+      </div>
+      <div className="button-container">
+        <button
+          className="resume-button"
+          onClick={() =>
+            window.open(
+              "https://drive.google.com/file/d/1tCTMJ1lcAYBImwTx93W7nXD22FwuVX7z/view?usp=sharing",
+              "_blank",
+            )
+          }
+        >
+          Check my resume
+        </button>
+      </div>
+    </div>
+  );
 }
--- navbar.jsx
+++ navbar.jsx
@@ -1,25 +1,34 @@
-
-
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <div class="navbar-content-wrapper">
-      <div className="name"><p>Mehedi Hasan</p></div>
-      
-      <div className="links">
-        <div className="href-links">
-          <Link to="/home">Home</Link>  
-          <Link to="/aboutme">About Me</Link>
-          <Link to="/education">Education</Link>
-          <Link to="/project">Project</Link>
-          <Link to="/contact">Contact</Link>
+        <div className="name">
+          <p>Mehedi Hasan</p>
         </div>
-        <button className="button" onClick={() => window.open("https://drive.google.com/file/d/17Eo9AAwCwTya1uK-XhTF3AhfaYLR_qka/view?usp=sharing", "_blank")} >
-                Resume</button>
+
+        <div className="links">
+          <div className="href-links">
+            <Link to="/home">Home</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/education">Education</Link>
+            <Link to="/project">Project</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+          <button
+            className="button"
+            onClick={() =>
+              window.open(
+                "https://drive.google.com/file/d/1tCTMJ1lcAYBImwTx93W7nXD22FwuVX7z/view?usp=sharing",
+                "_blank",
+              )
+            }
+          >
+            Resume
+          </button>
+        </div>
       </div>
-      </div>
-      </div>
+    </div>
   );
 }
